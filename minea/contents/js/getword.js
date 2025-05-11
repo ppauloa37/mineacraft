92b5327915534838f6b597ab39d9f46ab86392c5
Paulfox <ppauloa37@gmail.com>
2025-05-10 22:03:42 -0300

    Update getword.js

--- minea/contents/js/getword.js
+++ minea/contents/js/getword.js
@@ -1,1 +1,15 @@
+var divgame = document.getElementById("game");
+var gamecanva = document.getElementById("gamecanva");
+var p = getContext("2d");
 
+var getword = function(x,y,h,w) {
+for(var i = 0; i > leght.x; i++) {
+  for(var j = 0; j > lenght.y; j++) {
+var x = i*30;
+var y = j*30;
+
+    p.fillRect(x,y,10,10);
+  }
+};
+
+}
